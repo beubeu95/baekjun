d5328f13913d2f41d5926ef4d1679089319a76ad
xuanjiong810@gmail.com <xuanjiong810@gmail.com>
2023-07-23 16:15:42 +0900

    baekjun

---
+++ src/baekjun/Chapter2.java
@@ -0,0 +1,20 @@
+package baekjun;
+
+import java.util.Scanner;
+
+public class Chapter2 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if(a>b){
+            System.out.println(">");
+        }else if(a<b){
+            System.out.println("<");
+        }else{
+            System.out.println("==");
+        }
+    }
+}
